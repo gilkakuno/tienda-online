fdd740cbfac43e1e79d94c8880a042f3df2ea435
Gilka Lorena Kuno Apaza <gilkakuno@gmail.com>
2026-06-06 11:37:07 -0400

    tienda online

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,6 @@ import { OrdenProductoModule } from './orden-producto/orden-producto.module';
       database: process.env.DB_NAME || 'tienda_online',
       autoLoadEntities: true,
       synchronize: true,
-      // Configuración de SSL condicional: activa SSL en Render y lo desactiva en tu computadora local
       extra: process.env.DB_HOST
         ? {
           ssl: {
